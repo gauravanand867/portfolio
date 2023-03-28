e647a8f263898cc64b89dc217bffa1bf28c3f96b
Gaurav Anand <gauravanand867@gmail.com>
2023-03-28 12:44:12 +0530

    Updating Resume Link of View CV Button

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  // TODO: change your profile information here
   name: "Gaurav Anand",
   greeting: "Hey 👋",
   greetingDescription: "I'm Gaurav Anand and I'm a Software Engineer!",
   githubUrl: "https://github.com/gauravanand867",
   linkedinUrl: "https://www.linkedin.com/in/gauravanand867/",
   cvLink:
-    "https://docs.google.com/document/d/1E1234561NMyQA67890ygda7abcdefghijs1jBp7HFI0/",
+    "https://drive.google.com/file/d/1PA8cPq1_RJRilcEwt0aaGutkaVg7oNa-/view?usp=sharing",
 };
